991fc69d0d3853913b5d6b27f898436565c59e07
kfjustis <kfjustis@gmail.com>
2017-11-06 15:04:09 -0600

    Added files + setup for homework 3

---
+++ homework-3/justis_homework3.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+{-# OPTIONS_GHC -fno-warn-tabs #-}
+module Homework3 where
+
+import Test.Hspec
+import RPNAST
+
+
+prob1    :: a
+prob1    = undefined
+
+prob2    :: a
+prob2    = undefined
+
+prob3    :: a
+prob3    = undefined
+
+prob4    :: a
+prob4    = undefined
+
+-- Write your Hspec Tests below
