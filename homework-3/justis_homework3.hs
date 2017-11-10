3a7477d05df8e63d71948573d728e3cd0055f1c0
kfjustis <kfjustis@gmail.com>
2017-11-10 15:27:06 -0600

    Bad attempt, I'm feeding lists into a string slot...

--- homework-3/justis_homework3.hs
+++ homework-3/justis_homework3.hs
@@ -6,16 +6,30 @@ import Test.Hspec
 import RPNAST
 
 
-prob1    :: a
-prob1    = undefined
+prob1 :: String -> PExp
+prob1 (s) =
+    if length (s) == 1 then
+        if s == '+' then
+            [Plus]
+        else if s == '-' then
+            [Minus]
+        else if s == '*' then
+            [Mul]
+        else if s == '/' then
+            [IntDiv]
+    else
+        --let a:ab = words(x:xs)
+        --in prob1 (a) ++ [ab]
+        let x:xs = words s
+        in prob1 x ++ xs
 
-prob2    :: a
-prob2    = undefined
+prob2 :: a
+prob2 = undefined
 
-prob3    :: a
-prob3    = undefined
+prob3 :: a
+prob3 = undefined
 
-prob4    :: a
-prob4    = undefined
+prob4 :: a
+prob4 = undefined
 
 -- Write your Hspec Tests below
