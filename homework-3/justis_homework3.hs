5bfe858a235bc2d773889643c73b44c8a6646525
kfjustis <kfjustis@gmail.com>
2017-11-16 17:21:58 -0600

    Answer for prob2

--- homework-3/justis_homework3.hs
+++ homework-3/justis_homework3.hs
@@ -18,6 +18,18 @@ prob1 a = map stringToOp (words a)
         | otherwise = (Val (read (x:xs) :: Int))
 
 prob2 :: PExp -> Int
+prob2 a = hstack a []
+    where
+        hstack :: PExp -> [Int] -> Int
+        hstack ((Val i):rest) vals      = hstack rest (i:vals)
+        hstack (Plus:rest) (r:l:vals)   = hstack rest ((l + r):vals)
+        hstack (Minus:rest) (r:l:vals)  = hstack rest ((l - r):vals)
+        hstack (Mul:rest) (r:l:vals)    = hstack rest ((l * r):vals)
+        hstack (IntDiv:rest) (0:l:vals) = errorWithoutStackTrace "Cannot divide by 0"
+        hstack (IntDiv:rest) (r:l:vals) = hstack rest ((l `div` r):vals)
+        hstack [] [i]                   = i
+        hstack _ _                      = errorWithoutStackTrace "Unexpected case encountered"
+
 --prob2 [Val a] = a
 {-prob2 (x:xs) =
     if length (x:xs) >= 1 then
